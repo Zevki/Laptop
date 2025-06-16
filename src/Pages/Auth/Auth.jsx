9b6273d688cb91f230bb9f03bea01f407a3c56f6
Zevki <tqt2401@gmail.com>
2025-06-16 16:57:15 +0700

    First commit

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const Auth = () => {
+    return (
+        <div>
+            Auth Page
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Auth;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import Banner from "../../Components/Banner/Banner";
+import Delivery from "../../Components/Delivery/Delivery";
+import Brand from "../../Components/Brand/Brand";
+
+const Home = () => {
+  return (
+    <div>
+      {/* banner component */}
+      <div className="w-full min-h-[850px] bg-[#f0f2f3] flex items-center justify-center rounded-b-3xl">
+        <Banner></Banner>
+      </div>
+
+      {/* delivery component */}
+      <div className="delivery_component w-full min-h-[#150px]">
+        <Delivery></Delivery>
+      </div>
+
+      {/* brand component  */}
+      <div className="brand flex items-center h-[171px] justify-center w-full mt-8 mb-8">
+        <Brand></Brand>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
